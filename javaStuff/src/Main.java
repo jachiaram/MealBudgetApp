f7ac4a0525be2b7fdbcbe775d7fec27ebd5731fe
jachiaram <72281577+jachiaram@users.noreply.github.com>
2023-07-04 01:39:40 -0700

    Find Stores method implement

--- javaStuff/src/Main.java
+++ javaStuff/src/Main.java
@@ -1,25 +1,63 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        ArrayList<String> ingredients = new ArrayList<>();
-        findIngredients(ingredients);
-        System.out.println("");
+    public static void main(String[] args) throws FileNotFoundException, Exception {
+        String zipCode = "85142";
+        findStores(zipCode);
 
+        ArrayList<String> ingredients = findIngredients();
+        System.out.println("");
         for (String ingredient : ingredients) {
             System.out.println(ingredient);
         }
     }
 
+    public static String getHTML(String urlToRead) throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(conn.getInputStream()))) {
+            for (String line; (line = reader.readLine()) != null;) {
+                result.append(line);
+            }
+        }
+        return result.toString();
+    }
+
     // scrapingBee api call
+    public static void scrapingBee() {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(
+                        "http://dev.virtualearth.net/REST/v1/Locations/US//85142/locality/addressLine?includeNeighborhood=includeNeighborhood&include=includeValue&maxResults=1&key={Au4H8rRjUEFk8VuhpOG7T3pi-eY_uwG958-zQ5gpJxZCZL-8kaXiTFdrAw7zyEve}"))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(response.body());
+    }
 
     // convert html to txt file
 
     // takes in api call for website html, parses through the html to find the list
     // of ingredients and returns it as an
     // ArrayList.
-    public static ArrayList<String> findIngredients(ArrayList<String> arr) throws FileNotFoundException {
+    public static ArrayList<String> findIngredients() throws FileNotFoundException {
         FileReader in = new FileReader("./test.txt");
         BufferedReader reader = new BufferedReader(in);
         int count = 0;
@@ -31,7 +69,7 @@ public class Main {
                 // narrows down to lines that talk ab ingredients
                 if (line.indexOf("ingredients") != -1) {
                     temp.add(line);
-                    System.out.println(line);
+                    // System.out.println(line);
                     count++;
                 }
             }
@@ -40,7 +78,7 @@ public class Main {
             throw new RuntimeException(e);
         }
         System.out.println("");
-        System.out.println("Narrowed down to " + count + " lines of code.");
+        // System.out.println("Narrowed down to " + count + " lines of code.");
 
         // narrows down to one line
         String target = "";
@@ -49,7 +87,7 @@ public class Main {
                 target = temp.get(i);
             }
         }
-        System.out.println(target);
+        // System.out.println(target);
         System.out.println("");
         temp.clear();
 
@@ -69,27 +107,88 @@ public class Main {
                     if ((sub.indexOf(">") + 1) <= sub.length()) {
                         sub = sub.substring(sub.indexOf(">") + 1);
                     }
-                    if (arr.size() > 0) {
-                        if (!arr.get(k).equals(sub)) {
-                            arr.add(sub);
+                    if (temp.size() > 0) {
+                        if (!temp.get(k).equals(sub)) {
+                            temp.add(sub);
                             k++;
                         }
                     } else {
-                        arr.add(sub);
+                        temp.add(sub);
                         k++;
                     }
-                    System.out.println(sub);
+                    // System.out.println(sub);
                     count++;
                 }
             }
         }
-        System.out.println("There are " + count + " span elements in this line.");
-        System.out.println("Done.");
-        return arr;
+        // System.out.println("There are " + count + " span elements in this line.");
+        return temp;
+    }
+
+    // api call to find latitude and longitude of given zipcode
+    public static String findLatLong(String zipCode) throws Exception {
+        String geocodeRequest = getHTML(
+                "http://dev.virtualearth.net/REST/v1/Locations/US/-/" + zipCode
+                        + "/-/-?o=xml&key=Au4H8rRjUEFk8VuhpOG7T3pi-eY_uwG958-zQ5gpJxZCZL-8kaXiTFdrAw7zyEve");
+
+        int start = geocodeRequest.indexOf("<Latitude>") + 10;
+        int end = geocodeRequest.indexOf("</Latitude>");
+        String lat = geocodeRequest.substring(start, end);
+
+        start = geocodeRequest.indexOf("<Longitude>") + 11;
+        end = geocodeRequest.indexOf("</Longitude>");
+        String longitude = geocodeRequest.substring(start, end);
+        // System.out.println(geocodeRequest);
+        // System.out.println(lat);
+        // System.out.println(longitude);
+
+        geocodeRequest = lat + "," + longitude;
+        return geocodeRequest;
     }
 
     // api call to find stores nearby
-    public static ArrayList<String> findStores() {
+    public static ArrayList<String> findStores(String zipCode) throws Exception {
+        String coords = findLatLong(zipCode);
+        String nearbySearch = getHTML("https://dev.virtualearth.net/REST/v1/LocalSearch/?query=grocery&userLocation="
+                + coords + "&key=Au4H8rRjUEFk8VuhpOG7T3pi-eY_uwG958-zQ5gpJxZCZL-8kaXiTFdrAw7zyEve");
+
+        int start = nearbySearch.indexOf("al\":") + 4;
+        String estimatedTotal = nearbySearch.substring(start);
+        int end = estimatedTotal.indexOf(",");
+        estimatedTotal = estimatedTotal.substring(0, end);
+        int total = Integer.parseInt(estimatedTotal);
+        for (int i = 0; i < total; i++) {
+
+            int temp = nearbySearch.indexOf("SearchResult");
+            int tempEnd = nearbySearch.indexOf("Display");
+
+            // seperating search results into items
+            String item = nearbySearch.substring(temp, tempEnd);
+
+            // getting the name of the store
+            int nameStart = item.indexOf("name") + 7;
+            item = item.substring(nameStart);
+            int nameEnd = item.indexOf("\"");
+            String name = item.substring(0, nameEnd);
+            System.out.println(name);
+
+            // getting the address of the store
+            int addStart = item.indexOf("Line") + 7;
+            item = item.substring(addStart);
+            int addEnd = item.indexOf("\"");
+            String address = item.substring(0, addEnd);
+            System.out.println(address);
+
+            // retrieving website url of the store
+            int webStart = item.indexOf("Website") + 10;
+            item = item.substring(webStart);
+            int webEnd = item.indexOf("\"");
+            String website = item.substring(0, webEnd);
+            System.out.println(website);
+
+            System.out.println("");
+            nearbySearch = nearbySearch.substring(tempEnd + 10);
+        }
         return new ArrayList<>();
     }
 
@@ -98,7 +197,9 @@ public class Main {
     // ingredients found, 2D array might be needed here to present different options
     // to the user (group array ingredient
     // per row, store by column).
-    public static ArrayList<String> findPrices(ArrayList<String> Ingredients, ArrayList<String> Stores) {
+    public static ArrayList<String> findPrices(ArrayList<String> Ingredients) {
+
         return new ArrayList<>();
     }
+
 }
